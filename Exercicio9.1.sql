60ebe908dd39950e16d28c08c5e7711960fb77f0
Jessica-Curtinaz <jessicacurtinaz@gmail.com>
2025-05-27 23:32:48 -0300

    Create Exercicio9.1.sql

---
+++ Exercicio9.1.sql
@@ -0,0 +1,44 @@
+/* 1. a) Faça um resumo da quantidade vendida (Sales Quantity) de acordo com o nome do canal
+de vendas (ChannelName). Você deve ordenar a tabela final de acordo com SalesQuantity,
+em ordem decrescente.*/
+
+SELECT
+	ChannelName AS 'Canal de Venda',
+	SUM(SalesQuantity) AS 'Total Vendido'
+FROM
+	FactSales
+INNER JOIN DimChannel
+	ON FactSales.channelKey = DimChannel.ChannelKey
+GROUP BY ChannelName
+ORDER BY SUM(SalesQuantity) DESC
+
+
+/* b) Faça um agrupamento mostrando a quantidade total vendida (Sales Quantity) e
+quantidade total devolvida (Return Quantity) de acordo com o nome das lojas
+(StoreName).*/
+
+SELECT
+	StoreName AS 'Loja',
+	SUM(SalesQuantity) AS 'Total Vendido',
+	SUM(ReturnQuantity) AS 'Total Devolvido'
+FROM
+	FactSales
+INNER JOIN DimStore
+	ON FactSales.StoreKey = DimStore.StoreKey
+GROUP BY StoreName
+ORDER BY StoreName
+
+
+/* c) Faça um resumo do valor total vendido (Sales Amount) para cada mês
+(CalendarMonthLabel) e ano (CalendarYear).*/
+
+SELECT
+	CalendarYear AS 'Ano',
+	CalendarMonthLabel AS 'Mês',
+	SUM(SalesAmount) AS 'Faturamento Total'
+FROM
+	FactSales
+INNER JOIN DimDate
+	ON FactSales.DateKey = DimDate.Datekey
+GROUP BY CalendarYear, CalendarMonthLabel, CalendarMonth
+ORDER BY CalendarMonth ASC
